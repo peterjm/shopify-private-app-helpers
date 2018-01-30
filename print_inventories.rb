3cbd8fb34e5ffd6ce7c0dd1a22fdb243f3a6ee62
Peter McCracken <peter.mccracken@shopify.com>
2018-01-30 13:52:55 -0500

    update inventories script to work with any year

--- print_inventories.rb
+++ print_inventories.rb
@@ -1,43 +1,40 @@
 require 'csv'
 require_relative 'setup.rb'
 
-DATE_2017 = Time.parse("2017-01-01")
+def beginning_of_year(year)
+  Time.parse("#{year}-01-01")
+end
 
-def quantity_by_variant_id
-  @quantity_by_variant_id ||= begin
-    orders = orders_since(DATE_2017)
-    refunds = orders.map(&:refunds).flatten
+def fetch_quantities_sold_in_year(year)
+  orders = orders_since(beginning_of_year(year))
+  refunds = orders.map(&:refunds).flatten
 
-    qty = Hash.new(0)
+  qty = Hash.new(0)
 
-    orders.map(&:line_items).flatten.each_with_object(qty) do |line_item, qty|
-      qty[line_item.variant_id] += line_item.quantity
-    end
-
-    refunds.select(&:restock).map(&:refund_line_items).flatten.each_with_object(qty) do |refund_line_item, qty|
-      qty[refund_line_item.line_item.variant_id] -= refund_line_item.quantity
-    end
-
-    qty
+  orders.map(&:line_items).flatten.each_with_object(qty) do |line_item, qty|
+    qty[line_item.variant_id] += line_item.quantity
   end
+
+  refunds.select(&:restock).map(&:refund_line_items).flatten.each_with_object(qty) do |refund_line_item, qty|
+    qty[refund_line_item.line_item.variant_id] -= refund_line_item.quantity
+  end
+
+  qty
 end
 
-def quantity_sold_in_2017(variant)
-  quantity_by_variant_id[variant.id]
-end
-
-def print_inventory_file(filename, products)
+def print_inventory_file(filename, fiscal_year, products, quantities_sold_by_variant_id)
+  current_year = fiscal_year + 1
   CSV.open(filename, 'wb') do |csv|
     csv << [
       "Product ID",
       "Variant ID",
       "Product Title",
       "Current Inventory",
-      "Sold in 2017",
-      "Purchased in 2017",
-      "End 2016 Inventory",
+      "Sold in #{current_year}",
+      "Purchased in #{current_year}",
+      "End #{fiscal_year} Inventory",
       "Retail Price",
-      "End 2016 Total Value"
+      "End #{fiscal_year} Total Value"
     ]
     products
       .sort_by { |p| p.variants.map(&:inventory_quantity).max }
@@ -51,7 +48,7 @@ def print_inventory_file(filename, products)
         v.id,
         variant_title(v),
         v.inventory_quantity,
-        quantity_sold_in_2017(v),
+        quantities_sold_by_variant_id[v],
         0,
         nil,
         v.price,
@@ -61,16 +58,17 @@ def print_inventory_file(filename, products)
   end
 end
 
-def print_inventories()
+def print_inventories(year)
   all_products = fetch_all_products
+  quantities_sold_by_variant_id = fetch_quantities_sold_in_year(year+1)
 
   groceries = all_products.select { |p| %w(grocery).include?(p.product_type&.downcase) }
-  print_inventory_file("grocery_inventory.csv", groceries)
+  print_inventory_file("grocery_inventory.csv", year, groceries, quantities_sold_by_variant_id)
 
   non_groceries = all_products.select { |p| !%w(grocery workshop).include?(p.product_type&.downcase) }
-  print_inventory_file("non_grocery_inventory.csv", non_groceries)
+  print_inventory_file("non_grocery_inventory.csv", year, non_groceries, quantities_sold_by_variant_id)
 end
 
 if __FILE__ == $0
-  print_inventories()
+  print_inventories(2017)
 end
